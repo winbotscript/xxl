f14a11152fd7f6adae42c8d59149739f7c49cb5c
ariezta <42648618+ariezta@users.noreply.github.com>
2018-10-02 11:04:06 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ class LINE extends Command {
 
 
     get myBot() {
-        const bot = ['u52afe1d4ea5332242efacfeb9190d2a3','u58bc30a989f932d0fd73ccb847107779'];
+        const bot = ['u48b4ebd2407c6bb88df30b2645ddaa96'];
         return bot; 
     }
 
